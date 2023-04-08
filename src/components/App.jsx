72696701c3b06e465e85b2c295041865c1bf399e
SergeiTkachenko <fox.sergei92@gmail.com>
2023-04-08 14:46:06 +0200

    add start files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import { Layout } from './Layout';
+import { GlobalStyle } from './GlobalStyle';
+import { useSelector } from 'react-redux';
+import { AddForm } from './Form/Form';
+import { ContactList } from './ContactsList/ContactList';
+import { ContactFilter } from './ContactsFilter/ContactFilter';
+import { selectError, selectIsLoading } from 'redux/selectors';
+import Spinner from './Spinner/spinner';
+
 export const App = () => {
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <AddForm />
+      <ContactFilter />
+      {isLoading && !error && <Spinner />}
+      <ContactList />
+      <GlobalStyle />
+    </Layout>
   );
 };
